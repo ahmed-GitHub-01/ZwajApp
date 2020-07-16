1865c77b2b48ebeafb3cbbe936e39262432fb51d
ahmed <ahmed_20171531@icloud.com>
2020-07-16 12:37:56 +0300

    Initial Commit

---
+++ ZwajApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZwajApp.API.Data;
+
+namespace ZwajApp.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var Values = await _context.Values.ToListAsync();
+            return Ok(Values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValuesById(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.id == id);
+            return Ok(value);
+        }
+    }
+}
